dfd7711a23551cb9e5db3ad6d35c7e1ab7cfc694
coderNet2021 <cnss.saida@gmail.com>
2023-08-01 14:01:05 +0200

    reactive forms, and validation in the ts and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite/favorite.component';
@@ -10,12 +10,14 @@ import { LikeComponent } from './likeComponentTwitter/like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { UdemyCoursesFormComponent } from './udemy-courses-form/udemy-courses-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavoriteComponent,
     TitlecaseComponent,
+    SignupFormComponent,
     TitleCasePipe,
     LikeComponent,
     ZippyComponent,
@@ -24,7 +26,8 @@ import { UdemyCoursesFormComponent } from './udemy-courses-form/udemy-courses-fo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
