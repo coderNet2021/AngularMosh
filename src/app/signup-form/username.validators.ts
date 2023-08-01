e8b7e12b90b9c8f514c3ddd332d1a01765c95711
coderNet2021 <cnss.saida@gmail.com>
2023-08-01 21:32:31 +0200

    committing shouldBeUnique reactive form validator with asynchronous operations before correcting it later

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -7,4 +7,15 @@ export class UsernameValidators{
 
     return null;
   }
+
+  static shouldBeUnique(control: AbstractControl):ValidationErrors | null{
+    //if the username is mosh we are going to assume that it is taken, otherwise it is valid
+    setTimeout(()=>{
+      console.log('ok');
+    })
+    if(control.value==='mosh')
+      return {shouldBeUnique : true};
+
+      return null;
+  }
 }
