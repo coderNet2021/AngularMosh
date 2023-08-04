056360f67fb32ca8172869a8e8433ce19e5d233b
coderNet2021 <cnss.saida@gmail.com>
2023-08-04 06:34:25 +0200

    implementing the post method<Interface of Post>

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,12 +7,28 @@ import { HttpClient } from "@angular/common/http";
 })
 export class PostsComponent {
   posts :any[]=[];
-  constructor(httpClient : HttpClient){
-httpClient.get<any[]>('http://jsonplaceholder.typicode.com/posts').subscribe((response)=>{
+  private url='http://jsonplaceholder.typicode.com/posts';
+  constructor(private httpClient : HttpClient){
+httpClient.get<any[]>(this.url).subscribe((response)=>{
   console.log(response);
   this.posts=response;
   console.log(this.posts);
     });
   }
 
+  createPost(inputTitle:HTMLInputElement){
+    let post:Post = {id:0,title:inputTitle.value};
+    inputTitle.value='';
+    this.httpClient.post<Post>(this.url,post).subscribe((response)=>{
+      console.log(response);
+      post.id=response.id;
+      this.posts.splice(0,0,post);
+    });
+  }
+
+}
+
+ interface Post{
+   id:number;
+   title:string;
 }
