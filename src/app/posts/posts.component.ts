fe49a69f4772a15887beeec724c1d179549cfdb8
coderNet2021 <cnss.saida@gmail.com>
2023-08-04 07:59:54 +0200

    using the OnInit interface and ngOnInit method to initialise the component with data, and do not use the get method of httpClient in the constructor

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts :any[]=[];
   private url='http://jsonplaceholder.typicode.com/posts';
   constructor(private httpClient : HttpClient){
-httpClient.get<any[]>(this.url).subscribe((response)=>{
-  console.log(response);
-  this.posts=response;
-  console.log(this.posts);
-    });
+
+  }
+  ngOnInit(): void {
+    this.httpClient.get<any[]>(this.url).subscribe((response)=>{
+      console.log(response);
+      this.posts=response;
+      console.log(this.posts);
+        });
   }
 
   createPost(inputTitle:HTMLInputElement){
