672e1efe016dc47121d26e0b693c39cb5b69a068
coderNet2021 <cnss.saida@gmail.com>
2023-08-04 06:09:05 +0200

    implementing getting data from the server get<any[]>(url) and display the title

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -6,8 +6,13 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
+  posts :any[]=[];
   constructor(httpClient : HttpClient){
-
+httpClient.get<any[]>('http://jsonplaceholder.typicode.com/posts').subscribe((response)=>{
+  console.log(response);
+  this.posts=response;
+  console.log(this.posts);
+    });
   }
 
 }
