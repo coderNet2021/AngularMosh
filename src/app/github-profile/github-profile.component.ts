e4379c854fdd12c484644eb4591fdfab58730105
coderNet2021 <cnss.saida@gmail.com>
2023-08-06 14:13:26 +0200

    accessing the parameters using paramMap property on route object of type ActivatedRoute

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent {
+export class GithubProfileComponent implements OnInit{
+  constructor(private route : ActivatedRoute){
+
+  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params)=>{
+      console.log(params);
+      let id =params.get('id');
+      //in real scenario we want to send the id to get the profile like this :
+      // service.getProfile(id);
+      console.log(id);
+    });
+  }
 
 }
