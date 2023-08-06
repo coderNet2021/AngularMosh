2abe37d829967adc70ebe0932f3dc248059de474
coderNet2021 <cnss.saida@gmail.com>
2023-08-06 12:17:56 +0200

    implementing the routes - 1- Configuring the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, NgModule } from '@angular/core';
+import { Component, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,6 +18,11 @@ import { PostService } from './services/post.service';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { AppErrorHandler } from './common/app-error-handler';
 import { GithubFollowersService } from './services/github-followers.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -33,13 +38,24 @@ import { GithubFollowersService } from './services/github-followers.service';
     UdemyTopicsCoursesComponent,
     ChangePasswordComponentComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path:'',component :HomeComponent},
+      { path:'followers',component :GithubFollowersComponent},
+      { path:'profile/:username',component :GithubProfileComponent},
+      { path:'posts',component :PostsComponent},
+      { path:'**',component :NotFoundComponent}
+    ])
   ],
   providers: [
     PostService,
