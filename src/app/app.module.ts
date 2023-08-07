a33bd7b8cd5fdceb56e46002d7755ca14b7f14e8
coderNet2021 <cnss.saida@gmail.com>
2023-08-07 09:28:03 +0200

    dealing with multiple parameters in the app module and in routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,7 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       { path:'',component :HomeComponent},
       { path:'followers',component :GithubFollowersComponent},
-      { path:'followers/:id',component :GithubProfileComponent},
+      { path:'followers/:id/:username',component :GithubProfileComponent},
       { path:'posts',component :PostsComponent},
       { path:'**',component :NotFoundComponent}
     ])
