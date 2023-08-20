bdfb0e85fe8dbbccb1f44c36b0e8f8788e9af443
coderNet2021 <cnss.saida@gmail.com>
2023-08-20 19:58:51 +0200

    using CombineLatest to combine multiple observable into one, and observe this single one observable, and call the server to get all using these values of params

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, combineLatest } from 'rxjs';
+
 
 @Component({
   selector: 'app-github-followers',
@@ -13,15 +15,20 @@ export class GithubFollowersComponent implements OnInit{
 
   }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params=>{
-
+    //we combine the 2 observable into one observable using this combineLatest and we
+  // pass an array of observables
+    let newCombinedObservable = combineLatest([this.route.paramMap, this.route.queryParamMap]);
+    //we subscribe to the new observable
+    // and we do our staff -> we get the data from the server using these route params and optional paramss
+    newCombinedObservable.subscribe(combined=>{
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+      // this.service.getAll({id:id,page:page});
+      this.service.getAll().subscribe(followers=>{this.followers=followers; console.log(followers)});
     });
 
-    this.route.queryParamMap.subscribe(params=>{
 
-    });
 
-    this.service.getAll().subscribe(followers=>{this.followers=followers; console.log(followers)});
   }
 
 }
